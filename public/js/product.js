480d208d9af5abea54d8aeba4779a8b2575d983d
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-03-28 21:17:44 +0700

    update localStorage

--- public/js/product.js
+++ public/js/product.js
@@ -18,3 +18,45 @@ var mySwiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+let productPage_container = document.querySelector(".productPage_container");
+let productId = productPage_container.getAttribute("id");
+let productName = document.querySelector(".about_product > h1");
+let productPrice = document.querySelector(".about_product .price b");
+let arrayColor = document.querySelectorAll(".about_product .color span");
+console.log(arrayColor);
+let addToCartBtn = document.querySelector("#addToCart");
+let productColor = "";
+for (let i = 0; i < arrayColor.length; i++) {
+  productColor += arrayColor[i].innerText;
+}
+let productImage = document.querySelector(".swiper-slide img").src;
+let objectProduct = {
+  productId: Number(productPage_container.getAttribute("id")),
+  productName: productName.innerText,
+  productPrice: productPrice.innerText,
+  productColor: productColor,
+  productImage: productImage,
+};
+
+let getCartProduct = JSON.parse(
+  localStorage.getItem("ProductHasBeenAddedToCart")
+);
+if (getCartProduct == null) {
+  localStorage.setItem("ProductHasBeenAddedToCart", JSON.stringify([]));
+  window.location.reload();
+  console.log(getCartProduct);
+} else {
+  addToCartBtn.addEventListener("click", () => {
+    let setCartProduct = JSON.parse(
+      localStorage.getItem("ProductHasBeenAddedToCart")
+    );
+    setCartProduct.push(objectProduct);
+    localStorage.setItem(
+      "ProductHasBeenAddedToCart",
+      JSON.stringify(setCartProduct)
+    );
+    console.log("Hello");
+    console.log(objectProduct);
+  });
+}
